e8b559609f076548067701402f529bfb5cfabbbf
Prachijadhav1455 <127278802+Prachijadhav1455@users.noreply.github.com>
2023-07-19 21:44:06 +0530

    If_else

---
+++ Conditional_Statement/02 Using if else.c
@@ -0,0 +1,26 @@
+///Positive & negative no if else
+
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+   int No = 0;
+                
+   printf("\n Enter a Number = ");
+   scanf("%d",&No);
+                 
+   if(No > 0)
+    {
+      printf("\n welcome You Have Entered a Positive Number");
+    }
+    else
+    {        
+       printf("\n welcome You Hava Entered a Negative Number");
+    }
+      
+   getch();
+   printf("\n\n Thanks For interacting...."); 
+       
+  getch() ;
+  return 0 ;
+}
