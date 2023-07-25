60e00e8aeddd89a33b54e11819a7681d5b600cc3
Prachijadhav1455 <127278802+Prachijadhav1455@users.noreply.github.com>
2023-07-25 21:18:56 +0530

    While_Program

---
+++ Conditional_Statement/Using while.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+     int i = 0;
+     while(i < 10) 
+     {
+       printf("\n %d WELCOME",i+101);
+       i++;
+      
+     } printf("\nHello, World!");
+   printf("\n Thanks!!!!");
+   getch();
+   return 0;
+}
