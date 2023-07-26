b033c1b802d2d85cc98a36220aac7b076aee85ad
Prachijadhav1455 <127278802+Prachijadhav1455@users.noreply.github.com>
2023-07-26 21:38:39 +0530

    else_if_Program

---
+++ Conditional_Statement/Using else if.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+            int Month = 0;
+           printf("\n Enter Month Number (1-12) To View Month Text - ");
+           scanf ("%d",&Month);
+           if(Month==1)
+            {
+                     printf ("\n january");
+            }
+            else if (Month==2)
+            {
+                      printf ("\n February");
+            }
+            else if (Month == 3)
+            {
+                      printf ("\n March");
+            }
+            else if (Month == 4)
+            {
+                      printf ("\n April");
+            }
+           else if (Month == 5)
+            {
+                      printf ("\n May");
+            }
+            else if (Month == 6)
+            {
+                      printf ("\n jun");
+            }
+           else if (Month == 7)
+            {
+                      printf ("\n july");
+            }
+            else if (Month == 8)
+            {
+                      printf ("\n Augusta");
+            }
+           else if (Month == 9)
+            {
+                      printf ("\n Saptember");
+            }
+            else if (Month == 10)
+            {
+                      printf ("\n October");
+            }
+            
+           else if (Month == 11)
+            {
+                      printf ("\n November");
+            }
+           else if (Month == 12)
+            {
+                      printf ("\n December") ;
+            }
+            printf("\n Invalid Input");
+           
+             
+             
+   getch();
+  return 0;
+}
