427552682f586e341c32f1db1f41a470ae74b3df
Prachijadhav1455 <127278802+Prachijadhav1455@users.noreply.github.com>
2023-07-26 21:27:14 +0530

    do_while_Program

---
+++ Conditional_Statement/Using do while.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int cnt = 1;
+  
+  do
+  {
+    printf("\n Welcome");
+    printf("\n Hello ");
+    cnt ++;
+  }
+  while (cnt <=3);
+  
+  printf ("\n Thanks...!!!!");
+  
+  getch();
+  return 0;  
+  
+}
