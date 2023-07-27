6bdb9a951f37337733f8043317a025584b9b2e0a
Prachijadhav1455 <127278802+Prachijadhav1455@users.noreply.github.com>
2023-07-27 21:50:15 +0530

    For_Program

---
+++ Conditional_Statement/Using For.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int cnt=1;
+  for(cnt = 1;cnt<= 10;cnt++)
+  {
+    printf("\n Welcome");
+  }
+  printf("\n\n Thanks!!!!");
+  getch();
+  return 0;  
+}
