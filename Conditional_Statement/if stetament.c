425c179056ccefeeb9431940212f4220174dc1aa
Prachijadhav1455 <127278802+Prachijadhav1455@users.noreply.github.com>
2023-07-27 20:12:48 +0530

    Using_If_Stetament_Program

---
+++ Conditional_Statement/if stetament.c
@@ -0,0 +1,21 @@
+///Program To implement if stetament 
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  float Bill =0.0;
+  printf("\n Enter Your Balence =");
+  scanf ("%f",&Bill);
+  
+  if(Bill > 100)
+  {
+   printf("\n Welcome,You Are Eligible.");
+  }
+  
+  printf("\n\nGood Bye,Thanks For Visit");
+  
+  getch ();
+  return 0;
+ }
+ 
+
