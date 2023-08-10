02ea1be0abe30b1be13485b39719ebd13ec9338f
Prachijadhav1455 <127278802+Prachijadhav1455@users.noreply.github.com>
2023-08-10 21:55:52 +0530

    if_ele

---
+++ Conditional_Statement/if....else.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+  float Budget= 0.0;
+
+  printf("\n Enter Your Budget = ");
+  scanf("%f,&Budget");
+
+  if(Budget > 0)  //True or False
+ {
+   printf("\a\n Welcome To Showeoom!!!");
+   getch();
+ }
+ printf("\n Thanks For Visiting");
+
+ getch();
+ return 0;
+}
