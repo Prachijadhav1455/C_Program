381941e5cb2b8e6d3fa97f81e5c1819e288928bf
Prachijadhav1455 <127278802+Prachijadhav1455@users.noreply.github.com>
2023-08-11 22:42:36 +0530

    Putchar_Putch_Program

---
+++ Conditional_Statement/Using Putchar Putch.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+   char ch ='Q', Grade ='C', Group ='X';
+
+   printf("Welcome To Putch/Putchar Demonstartion =\n\n");
+   putchar('\n');
+   putchar('\n');
+
+   putchar(ch);
+   putch('\n');
+
+   putch(Group);
+   putch('\n');
+
+   putchar('s';
+   putch('\n');
+
+   putch('KARAD');
+   putch('\n');ss
+
+   putchar('Grade');
+   putch('\n');
+
+   getch();
+   return 0;
+
+}
