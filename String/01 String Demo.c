fbf2d4a96cac630d6e56072d7d03fa2ec2271c22
Prachijadhav1455 <127278802+Prachijadhav1455@users.noreply.github.com>
2023-08-16 20:05:04 +0530

    String_Demo_Program

---
+++ String/01 String Demo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+
+
+     char cArr[20]={'K','A','R','A','D'};                       ///char cArr[20] ="karad";
+
+     puts(cArr);
+
+                                                                   ///print("\n Enter a string
+     puts("Enter a string :");                                     ///scanf("%[^/n]",cArr);
+     gets(cArr);
+
+     puts("\n Given String is => ");                                 ///printf("/n Given String is => %s", cArr);
+     puts(cArr);
+
+     getch();
+     return 0;
+}
