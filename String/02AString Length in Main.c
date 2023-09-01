4f5e81bdb8c4fc76cff51e03d726cc0dbc94284c
Prachijadhav1455 <127278802+Prachijadhav1455@users.noreply.github.com>
2023-09-01 20:21:40 +0530

    String_Program

---
+++ String/02AString Length in Main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char cSrc[20] ={'\0'};
+    int i = 0;
+
+    printf("\n Enter a String :");
+    gets(cSrc);
+    {
+        while(i<=20)
+    {
+        break;
+    }
+    i++;
+
+    }
+    printf("\n Length Of Given String is = %d,i");
+
+    getch();
+    return 0;
+}
