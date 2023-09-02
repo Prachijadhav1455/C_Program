aea68421a3197cf1a9659f7ec3336376fd30a18c
Prachijadhav1455 <127278802+Prachijadhav1455@users.noreply.github.com>
2023-09-02 21:37:43 +0530

    String_Program

---
+++ String/02C String Program in main-Using printf().c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int main()
+{
+
+    char cSrc[50]= {'/0'};
+    int Len = 0;
+
+    printf("\n Enter a String :");
+    gets(cSrc);
+
+    Len = printf("%s",cSrc);
+
+    getch();
+    system("cls");
+
+    printf("\n Length Of Given String <%s> is = %d", cSrc , Len);
+
+    getch();
+    return  0;
+
+}
