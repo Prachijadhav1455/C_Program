2473838adf09777e0b503edd166699afaf937890
Prachijadhav1455 <127278802+Prachijadhav1455@users.noreply.github.com>
2023-09-04 21:39:22 +0530

    String_Program

---
+++ String/10 Count Capital Letters in  Given String -in Main.c
@@ -0,0 +1,30 @@
+// COunt Capital Letters in Given String
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char cSrc[50] = {'\0'};
+    char Cap_Cnt = 0,i = 0;
+
+
+    printf("\n Enter a String :");
+    gets(cSrc);
+
+    while(cSrc[i] != '\0')
+    {
+        if(cSrc[i] >= 'A' && cSrc[i] <= 'Z')
+        {
+            Cap_Cnt++;
+        }
+        i++;
+    }
+
+    printf("\n Counnt Capital LetTers Given String is = %d.",Cap_Cnt);
+
+    getch();
+    return 0;
+
+
+}
