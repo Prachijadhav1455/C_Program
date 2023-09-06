ea8951c876cf21ce55673d24384f4afda95d3b97
Prachijadhav1455 <127278802+Prachijadhav1455@users.noreply.github.com>
2023-09-06 08:57:15 +0530

    String_Program

---
+++ String/15 COnvert Given String To Upper Case in main.c
@@ -0,0 +1,28 @@
+//Convert Given String To Uppper Case
+
+#include<stdio.h>
+#include<conio.h>
+
+
+ int main()
+ {
+     char cSrc[20] ={'\0'};
+     int i =0;
+
+     printf("\n Enter a String =");
+     gets(cSrc);
+
+     while(cSrc[i] != '\0')
+     {
+         if(cSrc[i] >= 'a' && cSrc[i] <= 'z')
+         {
+             cSrc[i] -=32;                //cSrc[i] = cSrc[i] - 32;
+         }
+         i++;
+     }
+
+     printf("\n Given String is After Capiralization = %s.",cSrc);
+
+     getch();
+     return 0;
+ }
