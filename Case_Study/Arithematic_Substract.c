7b3f2bdb8ce0cca1c9a77655c8c7a7f496f86154
Prachijadhav1455 <127278802+Prachijadhav1455@users.noreply.github.com>
2023-10-03 19:51:10 +0530

    Arithematic_Substract

---
+++ Case_Study/Arithematic_Substract.c
@@ -0,0 +1,21 @@
+
+ int Substract()
+ {
+    int N1,N2,Sub;
+
+    printf("\n Enter Any Number :");
+    scanf("%d",&N1);
+
+    printf("\n Enter ANy Number :");
+    scanf("%d",&N2);
+
+    Sub = N1-N2;
+
+    printf("\n%d-%d=%d",N1,N2,Sub);
+
+    getch();
+    return 0;
+ }
+
+
+
