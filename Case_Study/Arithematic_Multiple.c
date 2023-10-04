5e8b5db6077c90f9b4edc68a59e17a27f223fa1b
Prachijadhav1455 <127278802+Prachijadhav1455@users.noreply.github.com>
2023-10-04 20:34:30 +0530

    Arithematic_Multiple

---
+++ Case_Study/Arithematic_Multiple.c
@@ -0,0 +1,21 @@
+
+ int Multiple()
+ {
+    int N1,N2,Multiple;
+
+    printf("\n Enter Any Number :");
+    scanf("%d",&N1);
+
+    printf("\n Enter ANy Number :");
+    scanf("%d",&N2);
+
+    Multiple = N1*N2;
+
+    printf("\n %d%d=%d",N1,N2,Multiple);
+
+    getch();
+    return 0;
+ }
+
+
+
