7fafb3b5d7fd29352ee9ca90e992de9c4840f468
Prachijadhav1455 <127278802+Prachijadhav1455@users.noreply.github.com>
2023-10-07 20:36:50 +0530

    Arithematic_Division

---
+++ Case_Study/Arithematic_Division.c
@@ -0,0 +1,18 @@
+
+ int Division()
+ {
+     int N1,N2,DIV;
+
+     printf("\n Enter Any Number:");
+     scanf("%d",&N1);
+
+     printf("\n Enter Any Number:");
+     scanf("%d",&N2);
+
+     DIV = N1/N2;
+
+     printf("\n %d/%d=%d",N1,N2,DIV);
+
+     getch();
+     return 0;
+ }
