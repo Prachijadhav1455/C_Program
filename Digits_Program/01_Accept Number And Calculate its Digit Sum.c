682f6f2bf5e0aff1d2bfd02fc8a65068d51f23ba
Prachijadhav1455 <127278802+Prachijadhav1455@users.noreply.github.com>
2023-10-29 21:01:32 +0530

    Digits_Program

---
+++ Digits_Program/01_Accept Number And Calculate its Digit Sum.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+   int No = 0,Dig = 0, Temp = 0, dsum = 0;
+
+   printf("\n Enter Positive Number :");
+   scanf("%d",&No);
+
+   Temp = No;
+   while(Temp > 0 )
+   {
+     Dig = Temp%10;;
+     dsum = dsum + Dig;
+     Temp = Temp /10;
+   }
+   printf("\n Digits sum Od %d is = %d.",No,dsum);
+
+   getch();
+   return 0;
+}
